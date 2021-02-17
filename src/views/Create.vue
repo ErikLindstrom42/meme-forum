73303e98768ea5359b74d1ceb92ae386e0f149cf
Erik Lindstrom <elindstrom42@gmail.com>
2021-02-16 19:02:42 -0600

    followed instructions and working pushing to firebase

---
+++ src/views/Create.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-container>
+    <v-form @submit.prevent="generateMeme">
+      <v-row justify="space-between">
+        <v-col>
+          <v-text-field
+            outlined
+            label="ImageURL"
+            type="text"
+            v-model="imageURL"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            outlined
+            label="Top Text"
+            type="text"
+            v-model="topText"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            outlined
+            label="Bottom Text"
+            type="text"
+            v-model="bottomText"
+          />
+        </v-col>
+      </v-row>
+      <v-row v-if="!showMeme" justify="center">
+        <v-btn :disabled="!imageURL" text color="primary" type="submit"
+          >Generate Meme!</v-btn
+        >
+      </v-row>
+    </v-form>
+    <div v-if="showMeme">
+      <Meme
+        class="mx-auto"
+        :top="topText"
+        :bottom="bottomText"
+        :imageURL="imageURL"
+        :width="800"
+      ></Meme>
+      <div class="text-center mt-3">
+        <v-btn type="button" @click="saveMeme" color="primary">
+          Save this meme
+        </v-btn>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Meme from "../components/Meme";
+import { db } from "../firebase";
+export default {
+  components: { Meme },
+  data() {
+    return {
+      imageURL: "",
+      topText: "",
+      bottomText: "",
+      showMeme: false,
+    };
+  },
+  methods: {
+    generateMeme() {
+      this.showMeme = true;
+    },
+    async saveMeme() {
+      await db.collection("memes").add({
+        topText: this.topText,
+        bottomText: this.bottomText,
+        imageURL: this.imageURL,
+        normalized: `${this.topText.toUpperCase()} ${this.bottomText.toUpperCase()}`,
+      });
+      this.$router.push("/feed");
+    },
+  },
+  mounted() {
+    console.log("Create component mounted");
+  },
+  beforeDestroy() {
+    console.log("Create component about to be destroyed");
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Feed.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <div v-for="meme in memes" :key="meme.id" class="py-5">
+      <meme
+        class="mx-auto"
+        :top="meme.topText"
+        :bottom="meme.bottomText"
+        :imageURL="meme.imageURL"
+      />
+    </div>
+  </v-container>
+</template>
+<script>
+import { db } from "../firebase";
+import Meme from "../components/Meme";
+
+export default {
+  components: { Meme },
+  data() {
+    return {
+      memes: [],
+    };
+  },
+  //On mount sets array named memes to all the memes in the "memes" collection... memememememe
+  mounted() {
+    db.collection("memes").onSnapshot((snap) => {
+      const memes = snap.docs.map((doc) => {
+        return {
+          ...doc.data(),
+          id: doc.id,
+        };
+      });
+      this.memes = memes;
+    });
+  },
+};
+</script>
+
+<style></style>
